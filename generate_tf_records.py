79594d9f0c9704d5f81f125324112a1a780ec360
prakhar171 <prakhar.jain_ug19@ashoka.edu.in>
2019-07-08 11:08:07 +0530

    origin-master

--- generate_tf_records.py
+++ generate_tf_records.py
@@ -29,7 +29,7 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'cheque':
+    if row_label == 'cheques':
         return 1
     else:
         None
